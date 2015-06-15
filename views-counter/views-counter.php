d608393c9c8b395a236b69bde585362b35a7babb
Claudio Sanches <contato@claudiosmweb.com>
2015-06-14 23:40:13 -0300

    Added views-counter.php initial file

---
+++ views-counter/views-counter.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: WordCamp BH - Views Counter
+ * Plugin URI: https://github.com/WordPressBeloHorizonte/hackathon-exemplos
+ * Description: Este plugin permite você countar views em seus posts.
+ * Author: WordCamp Belo Horizonte
+ * Author URI: http://belohorizonte.wordcamp.org/
+ * Version: 1.0.0
+ * License: GPLv2 or later
+ * Text Domain: views-counter
+ * Domain Path: languages/
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
